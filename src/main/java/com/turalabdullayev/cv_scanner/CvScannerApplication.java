ce81273cd8d5ad8f4e5144cf470f4fb8dd6ad08f
Tural20000 <tabdullayev020@gmail.com>
2026-05-22 19:48:26 +0400

    Initial commit

---
+++ src/main/java/com/turalabdullayev/cv_scanner/CvScannerApplication.java
@@ -0,0 +1,13 @@
+package com.turalabdullayev.cv_scanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvScannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvScannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/turalabdullayev/cv_scanner/CvScannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turalabdullayev.cv_scanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvScannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
